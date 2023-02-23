f0ac519eeda2c89ae104b31093b019567166ff31
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-02-23 12:30:38 +0530

    updating ./learning-shell

--- redirectors.sh
+++ redirectors.sh
@@ -6,12 +6,16 @@
 #2. Appends  -“>>” standard output
 #             “<<” standard input
 # 3. Merge   -“p >& q” Merges output from stream p with stream q
-#“p <& q” Merges input from stream p with stream q
+#            -“p <& q” Merges input from stream p with stream q
 
 #cd learning-Shell #changing directory
 
 ls >/tmp/out #stndout in tmp/out
 cat /tmp/out #shows the content similar to the learning-shell .
-cd ..
-ls >>/tmp/out
+cd .. #coming out of the directory
+ls >>/tmp/out #appends  the output
+
+#stdout (1>)(>) -only output
+#stderr(2>) - only error
+#stdout&stderr(&>) -both outpout and error
 
