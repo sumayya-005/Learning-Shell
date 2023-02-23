4c32e0c0edd3dea1c201eae9cbafe63cc1b27ed9
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-02-23 11:32:17 +0530

    updating ./learning-Shell

---
+++ redirectors.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Redirection here simply means diverting the output or input.
+#1. Overwrite  -“>” standard output
+#               “<” standard input
+#2. Appends  -“>>” standard output
+#             “<<” standard input
+# 3. Merge   -“p >& q” Merges output from stream p with stream q
+#“p <& q” Merges input from stream p with stream q
