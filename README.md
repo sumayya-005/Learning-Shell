3e1682d51b3996391ebfe6f1c308e5a729ddf272
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-02-23 11:01:08 +0530

    updating ./learning-Shell

--- README.md
+++ README.md
@@ -3,12 +3,12 @@
 
 # Shell scripting
 # List of shell scripting topics
-1. Shebong and Comments
+1. Shebang and Comments
 2. Print 
 3. Redirectors and quotes
 4. Iputs
-5. functions
-6.  Exit Status
+5. functions 
+6. Exit Status
 7. conditions
 8. loops
 9. SED Editor
--- She-bang.sh
+++ She-bang.sh
@@ -1,2 +1,8 @@
-#! She-bang
-# Shell scripting is nothing but adding all your manual commands in order.
+#!/bin/bash
+
+#shebang is a comment which represents the shell type.
+#Shell scripting is nothing but adding all your manual commands in order.
+ls
+
+#'.sh' or '.bash' will be given as extention for the file.
+# bash <filename> runs the script.
