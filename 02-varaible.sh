727060af5a452f21242787f8642f26e9863655b2
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-02-24 11:28:41 +0530

    updating ./learning-shell

--- 02-varaible.sh
+++ 02-varaible.sh
@@ -1,6 +1,6 @@
-a=10
-b=abc
-c=true
+a=10 #integer
+b=abc #string
+c=true #boolean
 
 echo Value of a = $a
 
@@ -11,3 +11,10 @@ echo Today date is $DATE
 #Arithematic substitution
 ADD=$((2+3+4))
 echo Addition is $ADD
+
+#Variables names can have only characters a-z,A-Z,0-9_(underscore)
+#Special Characters are not allowed
+#A variable should not start with a number and it can start with a Underscore.
+#Everything is a string.
+#As a user you should know that what data would come,since there is no data types.
+
