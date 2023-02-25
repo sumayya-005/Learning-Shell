72df03bb3609bbbc0661fcd5ae16aa7c0e131683
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-02-25 11:01:15 +0530

    updating ./learning-shell

--- exercise.sh
+++ exercise.sh
@@ -4,9 +4,9 @@ if [ -e "$FILE" ]
   then
      echo "$FILE passwords are enabled."
 fi
-if [ -x “$FILE” ]
+if [ -x "$FILE" ]
   then
-    echo “You have permition to execute $FILE”
+    echo "You have permission to execute $FILE"
   else
-    echo “You do Not have permissions to execute $FILE”
+    echo "You do Not have permissions to execute $FILE"
 fi
