9447c5882b3d9e5733dd20759610154747d12dcc
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-02-25 10:50:28 +0530

    updating ./learning-shell

--- Exercise.sh
+++ Exercise.sh
@@ -6,9 +6,9 @@ if [ -e "$FILE" ];then
 echo "$FILE passwords are enabled"
 fi
 
-if [ -x "$FILE" ];then
-  echo "You have permissions to edit the file."
-else
-  echo "You don't have permissions to edit the file."
-
-fi
+#if [ -x "$FILE" ];then
+#  echo "You have permissions to edit the file."
+#else
+#  echo "You don't have permissions to edit the file."
+#
+#fi
