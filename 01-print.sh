4ac508c39608397e5049305ef2c4f05e775ef798
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-02-26 17:35:59 +0530

    updating ./learning-shell

--- 01-print.sh
+++ 01-print.sh
@@ -1,3 +1,8 @@
 echo hello world
 echo welcome to shell scripting.
 echo shell-scripting
+
+#Color pirnt syntax
+#echo -e "\e[COLmMESSAGE\e[0m]"
+#Ex. echo -e "\e[31mHELLO\e[0m"
+#\e[31m -> to enable color code 31
