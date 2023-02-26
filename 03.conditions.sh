04404aed598932c7a85fa481af29f4987ddc7922
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-02-26 17:17:08 +0530

    updating ./learning-shell

--- 03.conditions.sh
+++ 03.conditions.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+#expressions
+#string Comparision
+
+x="abc"
+if [ $x == abc ]; then
+  echo "yes both are equal"
+fi
+
+a=$1
+ if [ $a -gt 10 ];then
+    echo "$a is greater than 10"
+ else
+   echo "$a is less than 10"
+fi
+
+
+#file exist
+file=/tmp/new
+if [ -f $file ];then
+  echo file exists
+  else
+    echo file does not exist
+fi
+
+
 read -p 'Enter age :  ' age
  if [ $age -gt 18 ];then
     echo "You are eligible to vote"
