b31015cb7a182650b4e5d366655f7fe45ffd0909
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-02-26 16:53:10 +0530

    updating ./learning-shell

--- 02-varaible.sh
+++ 02-varaible.sh
@@ -26,8 +26,3 @@ echo ${b(0)}
 #Local property
 #environment Variables
 
-#Arguments
-echo $0 #Gives the saved file name
-echo $1 #Prints the first statement.
-echo $2 #prints the next statement.
-# we can print like this upto $9
